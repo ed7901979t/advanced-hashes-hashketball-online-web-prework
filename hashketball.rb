580e6be49551183088f3d849d1abd0f391423aa0
clean-command-1851 <ed7901979t@gmail.com>
2019-08-03 01:36:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,14 @@
  def home_team_name
   game_hash {
-    :home => "Alan Anderson"
-    :team_name => "0"
-    :players =>
-    :colors  =>
+    :home =>
+    :players => "Alan Anderson"
+    :team_name => "0" 
      
-    :number =>   , 
-    :shoe =>   , 
-    :points =>  
+    :colors  =>"Black"
+     
+    :number => "0"   , 
+    :shoe =>   , "16"
+    :points =>  "22"
     :rebounds =>
     :assists =>
     :steals =>
