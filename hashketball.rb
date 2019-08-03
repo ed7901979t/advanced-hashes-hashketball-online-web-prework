a5a7527549070feb0e8703d20eed8983d46f7990
clean-command-1851 <ed7901979t@gmail.com>
2019-08-03 00:14:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,17 @@
   game_hash {:home { 
     :team_name { "Brooklyn Nets"  }
     :colors {"Black", "White" }
-    :players {:number =>   , :shoe =>   , :points }
+    :players {
+    :number =>   , 
+    :shoe =>   , 
+    :points =>  
+    :rebounds =>
+    :assists =>
+    :steals =>
+    :blocks =>
+    :slam_dunks =>
+      
+    }
     
   
     
